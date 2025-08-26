77bca9c3d17474bc84065fada65d6805d523e2cb
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-08-27 00:21:42 +0530

    Modified

--- tricky.py
+++ tricky.py
@@ -7,6 +7,27 @@ print(5//2)  //2
 #Division
 print(5/2)   //2.5
 
+#String 
+a="12"
+a+="1"
+print(int(a)*2)  //242
+
+a="12"
+print(a*3+"5")  //1212125
+
+a="12"
+print(a*3+5)  //KeyError
+
+#a^AnyNum (a**n)
+print(5**(2**3))   //5**(8)=5^8
+
+print((2**2)**3)   //4^3=48
+
+print(2**2**1)   #Base(2)^2^1=4
+
+
+
+
 
 
 
