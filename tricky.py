96ef7d3e2897ee955597b817be0d1e61eaafa561
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-09-02 23:36:55 +0530

    Modified tricky.py file

--- tricky.py
+++ tricky.py
@@ -9,8 +9,8 @@ print(5/2)   //2.5
 
 #String 
 a="12"
-a+="1"
-print(int(a)*2)  //242
+a+="1"          //a = a+1 = "12"+"1"
+print(int(a)*2)  //121*2=242
 
 a="12"
 print(a*3+"5")  //1212125
@@ -28,7 +28,7 @@ print(2**2**1)   #Base(2)^2^1=4
 #String Manipulation
 a="1"
 b="2"
-print(int(a+b)+int(b+a))  #
+print(int(a+b)+int(b+a))  #12+21=33
 
 
 
