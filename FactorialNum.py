1a0db8ad69ff57b49dca229f9d845aefc86824df
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-09-10 23:08:57 +0530

    Modified

--- FactorialNum.py
+++ FactorialNum.py
@@ -1,6 +1,7 @@
+#Factorial of Single Number
 def factorial(n)
     result = 1
     for i in range(1,n+1):
         result*=i
         return result
-    print("5!=",factorial(5))
+    print("5!=",factorial(5))    #120
