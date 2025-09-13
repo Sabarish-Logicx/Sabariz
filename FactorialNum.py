e112572bda5805a963b89c6f22a39573960fb653
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-09-13 23:59:35 +0530

    Modified

--- FactorialNum.py
+++ FactorialNum.py
@@ -1,18 +1,22 @@
 #Factorial of Single Number
 
-num=int(input("Enter a Number: "))
-
-def factorial(n)
+def factorial(n):
     result = 1
     for i in range(1,n+1):
         result*=i
         return result
     print("5!=",factorial(5))    #120
 
+#Factorial for Given input numbers
 
-#
-num=5
+num=int(input("Enter a Number: "))
+
+#num=5
+
 factorial=1
 for i in range(1,num+1):
     factorial*=i
     print(factorial)
+
+
+
