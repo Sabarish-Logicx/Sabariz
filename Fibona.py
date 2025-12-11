02b995a63478199016c6228e34d53beb900e463c
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-12-11 23:04:26 +0530

    Createded Fibonat.py

--- Fibona.py
+++ Fibona.py
@@ -1,11 +1,13 @@
 #Fibonaaci Series
 
+n=int(input("Enter the value : "))
+
 n=10
 a,b=0,1
 
 for i in range(n):
-    print(a,end=" ")
-    a,b=b,a+b         #Update values
+    print(a,end=" ")   #0 1 1 2 3 5 8 13 21 34 
+    a,b=b,a+b          #Update values
 
 #Swap 2 num
 temp=a
