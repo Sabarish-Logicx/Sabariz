9b2c67c68d660210805dbab29c8032d9c7a6b8e2
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-12-19 23:34:34 +0530

    Created Reverse.py

--- Reverse.py
+++ Reverse.py
@@ -4,3 +4,11 @@ a=[1,2,3,4]
 a.reverse()
 print(a)          #[4,3,2,1]
 
+#Reverse Using Slicing 
+[] list       a=[1,2,3,4]
+() tuple      b=(1,2,3,4)
+" " String    s="SABARISH"
+
+print(a[::-1])     #[4,3,2,1]
+print(b[::-1])     #(4,3,2,1) 
+print(s[::-1])     #HSIRABAS
